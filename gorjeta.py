cce46e3b657f53ce2df26f8b14d9510731be9aae
Eduardo Arruda <eduardoarruda2001@outlook.com>
2024-01-15 23:43:03 -0300

    English

--- gorjeta.py
+++ gorjeta.py
@@ -1,7 +1,7 @@
-print("Calculadora de Conta e Gorjeta")
-Conta = float(input("Qual o valor total da conta?\n"))
-Porcentagem = float(input("Qual porcetagem em gorjeta você deseja dar?\n"))
-Pessoas = float(input("Quantas pessoas vão dividir a conta?\n"))
+print("Bill and Tip Calculator")
+Conta = float(input("What is the total value of the bill?\n"))
+Porcentagem = float(input("What percentage of tip do you want to give?\n"))
+Pessoas = float(input("How many people will split the bill?\n"))
 
 Porcentagem1 = Porcentagem / 100 * Conta
 Gorjeta = Porcentagem1 / Pessoas
@@ -9,7 +9,7 @@ Gorjeta = Porcentagem1 / Pessoas
 Conta1 = Conta / Pessoas - Gorjeta
 Gorjeta2 = Gorjeta / Pessoas
 
-print("Cada pessoa pagará: " + str(round(Conta1,2)))
-print("O valor da gorjeta será: " + str(round(Gorjeta2,2)))
+print("Each person will pay: " + str(round(Conta1,2)))
+print("The tip amount will be: " + str(round(Gorjeta2,2)))
 
-enter = input("Aperte Enter para fechar")
+enter = input("Press Enter to close")
